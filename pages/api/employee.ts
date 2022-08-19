435ebd988cabd48157e12a20f1af9225e15fe931
Balu Sai <balu11698@gmail.com>
2022-08-19 17:25:08 +0530

    netlify test

--- pages/api/employee.ts
+++ pages/api/employee.ts
@@ -7,6 +7,7 @@ import {
 import Cors from "cors";
 import corsOptions from "../../database/config/corsOptions";
 import middleware from "../../database/middlware/middleware";
+import axios from "axios";
 
 const cors = Cors(corsOptions);
 
@@ -14,6 +15,10 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<any>
 ) {
-  await middleware(req, res, cors);
-  return getAnimeData(req, res);
+  // await middleware(req, res, cors);
+  // return getAnimeData(req, res);
+  const response = await axios.get(
+    "https://api.jikan.moe/v4/seasons/now?page=1"
+  );
+  return res.send(response.data);
 }
